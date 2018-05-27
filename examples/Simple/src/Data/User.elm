4dacd12e51d670c9b4aafa8220df6d9218f15523
Tereza Sokol <terezasokol@gmail.com>
2018-05-27 18:12:09 -0500

    missing formatting

--- examples/Simple/src/Data/User.elm
+++ examples/Simple/src/Data/User.elm
@@ -1,34 +1,40 @@
 module Data.User exposing (User, request, setColor)
 
+{-|
+
+@docs User, request, setColor
+
+-}
+
 import Http
 import Json.Decode
 
 
 {-| -}
 type alias User =
-    { id : Int
-    , name : String
-    , username : String
-    , settings : Settings
-    }
+  { id : Int
+  , name : String
+  , username : String
+  , settings : Settings
+  }
 
 
 {-| -}
 type alias Settings =
-    { color : String
-    }
+  { color : String
+  }
 
 
 {-| -}
 request : Int -> Http.Request User
 request id =
-    Http.get ("https://jsonplaceholder.typicode.com/users/" ++ toString id) decoder
+  Http.get ("https://jsonplaceholder.typicode.com/users/" ++ toString id) decoder
 
 
 {-| -}
 setColor : String -> User -> User
 setColor color ({ settings } as user) =
-    { user | settings = { settings | color = color } }
+  { user | settings = { settings | color = color } }
 
 
 
@@ -37,16 +43,16 @@ setColor color ({ settings } as user) =
 
 decoder : Json.Decode.Decoder User
 decoder =
-    Json.Decode.map3 init
-        (Json.Decode.field "id" Json.Decode.int)
-        (Json.Decode.field "name" Json.Decode.string)
-        (Json.Decode.field "username" Json.Decode.string)
+  Json.Decode.map3 init
+    (Json.Decode.field "id" Json.Decode.int)
+    (Json.Decode.field "name" Json.Decode.string)
+    (Json.Decode.field "username" Json.Decode.string)
 
 
 init : Int -> String -> String -> User
 init id name username =
-    { id = id
-    , name = name
-    , username = username
-    , settings = Settings "lightpink"
-    }
+  { id = id
+  , name = name
+  , username = username
+  , settings = Settings "lightpink"
+  }
